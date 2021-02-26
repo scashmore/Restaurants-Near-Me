edcd8aabe51b680fa37de9a507476b9adfb7d220
Shawn Fox <78384399+SFoxGit@users.noreply.github.com>
2021-02-26 14:56:50 -0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,1 @@
-//js
-var closeBtnEl = $('#closeBtn');
-var goingBtnEl = $('#goingBtn');
-var notGoingBtn = $('#notGoingBtn');
 
-closeBtnEl.on('click', function() {};
-goingBtnEl.on('click', function() {};
-notGoingBtn.on('click', function() {};
