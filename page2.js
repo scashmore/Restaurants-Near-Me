476f0388e17fe41382b73faebcbdb17354922729
Uriel Rosario <urielrosario1010@gmail.com>
2021-02-26 12:46:01 -0500

    testing commit

---
+++ page2.js
@@ -0,0 +1,20 @@
+var visitedEl = $('#visited');
+var notVisitedEl = $('#not-visited');
+var cookedEl = $('#cooked');
+var notCookedEl = $('#not-cooked');
+
+
+
+
+
+visitedEl.on('click', function () {
+    console.log("its clicked")
+}
+notVisitedEl.on('click', function () {}
+cookedEl.on('click', function () {}
+notCookedEl.on('click', function () {}
+
+
+
+
+
