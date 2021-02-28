16e0f28a2f41c071878f64267f2f59ab05bf632c
Shawn Fox <foxeasttrojan06@gmail.com>
2021-02-28 00:31:22 -0500

    pages 1 and 3 fully functioning including local storage

--- assets/js/page3.js
+++ assets/js/page3.js
@@ -9,44 +9,100 @@
 // notGoingBtn.on('click', function() {};
 
 //Shawn's code
-var getRest = function(city) {
-   
-    fetch("https://documenu.p.rapidapi.com/restaurants/zip_code/" + city + "?size=100&fullmenu=false", {
-	"method": "GET",
-	"headers": {
-		"x-api-key": "e9fe0dd92c9b93bedf0eff314991856e",
-		"x-rapidapi-key": "88e1447b5fmsh82bcb1516d4eca7p18ca6bjsn49a0d0de2e35",
-		"x-rapidapi-host": "documenu.p.rapidapi.com"
-	}
-})
-        .then(function(response) {
+var getRest = function (city) {
+
+    fetch("https://documenu.p.rapidapi.com/restaurants/zip_code/" + city + "?size=100&fullmenu=true", {
+        "method": "GET",
+        "headers": {
+            "x-api-key": "e9fe0dd92c9b93bedf0eff314991856e",
+            "x-rapidapi-key": "88e1447b5fmsh82bcb1516d4eca7p18ca6bjsn49a0d0de2e35",
+            "x-rapidapi-host": "documenu.p.rapidapi.com"
+        }
+    })
+        .then(function (response) {
+            randomRest = "" + (Math.floor(Math.random() * 19));
             if (response.ok) {
-                response.json().then(function (data) {
-                    console.log(data);
-                    randomRest = data[(Math.floor(Math.random() * 19))];
+                response.json().then(function (rests) {
+                    console.log(rests);
                     console.log(randomRest);
-                    // writeRest(randomRest);
+                    console.log(rests.data[randomRest].address.formatted);
+                    writeRest(rests, randomRest);
                 });
             }
         })
-        .catch(function(error) {});
+        .catch(function (error) { });
 };
 
-getRest();
+var testing = JSON.parse(localStorage.getItem('restZip'));
+var zip = testing[Object.keys(testing)[Object.keys(testing).length - 1]];
+getRest(zip);
 
-// var writeRest = function(data) {
-//     var name =
-//     var address = 
-//     var 
-// }
+var first = "0"
+var writeRest = function (rests, randomRest) {
+    var picked = rests.data[randomRest];
+    var name = picked.restaurant_name;
+    var address = picked.address.formatted;
+    var restId = picked.restaurant_id;
+    var menu = picked.menus[0]["menu_sections"];
+    console.log(menu);
+    $('#result').append('<div id="restName">' + name + '</div>');
+    $('#result').append('<div id="restAddress">' + address + '</div>');
+    // Make this not dipslay, will pull from to save id to local storage for review page
+    $('#result').append('<div id="restId" data-restID="' + restId + '"></div>');
+    for (var i = 0; i < menu.length; i++) {
+        var sectionName = menu["" + i]["section_name"];
+        var sectionDisc = menu["" + i]["description"];
+        $('#menuPrint').append('<div class="sectionName">' + sectionName + '</div>');
+        $('#menuPrint').append('<div class="sectionDisc">' + sectionDisc + '</div>');
+        var test = Number(menu[i]["menu_items"]['length'])
+        for (var j = 0; j < test; j++) {
+            var menuItemArr = menu[i]["menu_items"][j]
+            var dishName = menuItemArr["name"];
+            var dishDisc = menuItemArr["description"];
+            var dishPrice = menuItemArr["price"];
+            $('#menuPrint').append('<div class="dishName">' + dishName + '</div>');
+            $('#menuPrint').append('<div class="dishDisc">' + dishDisc + '</div>');
+            if (dishPrice != 0) {
+                $('#menuPrint').append('<div class="dishPrice">$ ' + dishPrice + '</div>');
+            };
+        }
+    }
+
+}
 
 //reruns the randomizer
-$('#notGoingBtn').on('click', function() {
+$('#notGoingBtn').on('click', function (event) {
     //clears result div before running script again
     $('#result').empty();
-    getRest();
+    $('#menuPrint').empty();
+    getRest(zip);
 })
 
-$('#goingBtn').on('click', function() {
+$('#goingBtn').on('click', function (event) {
     // need save to local storage savedRestaurants name + ID
+    var rest = $('#restName').text();
+    var restId = $('#restId').attr("data-restId");
+    var pullLocal = localStorage.getItem('visitedRestaurants');
+    var edited = JSON.parse(pullLocal);
+    console.log(firstName);
+    console.log(firstId);
+    if (pullLocal === null) {
+        var firstObj = new Object();
+        firstObj[rest] = restId;
+        localStorage.setItem('visitedRestaurants', JSON.stringify(firstObj));
+        console.log(JSON.stringify(firstObj));
+    } else if (Object.keys(edited).length === 1) {
+        var firstName = Object.keys(edited)[0];
+        var firstId = edited[Object.keys(edited)[0]];
+        var newObj = new Object();
+        newObj[rest] = restId;
+        newObj[firstName] = firstId;
+        localStorage.setItem('visitedRestaurants', JSON.stringify(newObj));
+    } else {
+        var current = JSON.parse(localStorage.getItem('visitedRestaurants'));
+        current[rest] = restId
+        localStorage.setItem('visitedRestaurants', JSON.stringify(current));
+        console.log(JSON.parse(localStorage.getItem('visitedRestaurants')));
+    }
 })
+
