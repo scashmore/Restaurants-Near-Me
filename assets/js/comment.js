ec2fe7c249f6083e809a912fe92b62a333d87483
Susan Ashmore <scashmorecitcom@gmail.com>
2021-03-01 23:04:16 -0500

    CPU Style

--- assets/js/comment.js
+++ assets/js/comment.js
@@ -1,77 +1,150 @@
+var pullLocalRest = JSON.parse(localStorage.getItem('visitedRestaurants')) || {};
+var pullLocalRec = JSON.parse(localStorage.getItem('savedMeals')) || {};
 //on page load, write most recent saved rest
 var mostRecent = function (recentRestId) {
-    var pullLocal = localStorage.getItem('visitedRestaurants');
-    var edited = JSON.parse(pullLocal);
-    var storedLength = Object.keys(edited).length;
-    var lastEl = storedLength - 1;
-    var mostRecentName = Object.keys(edited)[lastEl];
+    var storedLength = Object.keys(pullLocalRest).length;
+    var lastId = pullLocalRest.last;
+    var mostRecentName = pullLocalRest[lastId].name;
+    var mostRecentCom = pullLocalRest[lastId].comment;
+    var mostRecentRate = pullLocalRest[lastId].rating;
+    console.log(mostRecentName);
     $('#restPrint').text(mostRecentName);
-    $('#restPrint').append('<div class="restId" data-restID="' + recentRestId + '"></div>')
-    for (var i = lastEl - 1; i > -1; i--) {
-        console.log(Object.keys(edited)[i]);
-        var nextRest = Object.keys(edited)[i];
-        console.log(nextRest);
-        var nextId = Object.values(edited)[i][0];
-        console.log(edited);
-        console.log(nextId);
-        var nextComment = Object.values(edited)[i][1];
-        var nextRating = Object.values(edited)[i][2];
-        console.log(nextId);
-        console.log(nextComment);
-        //Yes I'm using template literal for this
-        var inputCard = `<section class="card uk-margin">
-        <div class="restId" id="restName${i}">${nextRest}
-        <div class="restId" data-restID="${nextId}"></div>
-        </div>
-        <form class="textinput">
-            <textarea row="5" class="uk-input" type="text"
-                placeholder="Comments"></textarea>
-
-
-            <div class="stars" action="">
-                <input class="star star-5 save" id="${i}star-5" type="radio" name="star${i}" value="5"/>
-                <label class="star star-5" for="${i}star-5"></label>
-                <input class="star star-4 save" id="${i}star-4" type="radio" name="star${i}" value="4"/>
-                <label class="star star-4" for="${i}star-4"></label>
-                <input class="star star-3 save" id="${i}star-3" type="radio" name="star${i}" value="3"/>
-                <label class="star star-3" for="${i}star-3"></label>
-                <input class="star star-2 save" id="${i}star-2" type="radio" name="star${i}" value="2"/>
-                <label class="star star-2" for="${i}star-2"></label>
-                <input class="star star-1 save" id="${i}star-1" type="radio" name="star${i}" value="1"/>
-                <label class="star star-1" for="${i}star-1"></label>
-            </div>
-            <button class="restSubmit btn uk-button uk-button-default">Save</button>
-        </form>
-        <button class="goAgain btn uk-button uk-button-default>Get Directions</button>
-    </section>`
-        $('#allRestPrint').append(inputCard);
-        if (nextRating == 5) {
-            var starPop = i + "star-5";
-            $('#' + starPop).prop("checked", "checked");
-        } else if (nextRating == 4) {
-            var starPop = i + "star-4";
-            $('#' + starPop).prop("checked", "checked");
-
-        } else if (nextRating == 3) {
-            var starPop = i + "star-3";
-            $('#' + starPop).prop("checked", "checked");
-
-        } else if (nextRating == 2) {
-
-            var starPop = i + "star-2";
-            $('#' + starPop).prop("checked", "checked");
-        } else if (nextRating == 1) {
-            var starPop = i + "star-1";
-            $('#' + starPop).prop("checked", "checked");
-
+    $('#restPrint').append('<div class="restId" data-restID="' + lastId + '"></div>')
+    var keyArr = Object.keys(pullLocalRest);
+    var i = 1;
+    keyArr.forEach(element => {
+        console.log(element);
+        if (element !== "last") {
+            var nextId = element;
+            var nextRest = pullLocalRest[element].name;
+            var nextComment = pullLocalRest[element].comment;
+            console.log(nextComment);
+            var nextRating = pullLocalRest[element].rating;
+            var lat = pullLocalRest[element].latitude;
+            var lon = pullLocalRest[element].longitude;
+            //Yes I'm using template literal for this
+            var inputCard = `<section class="cardC uk-margin">
+                <div class="restId" id="restName${i}">${nextRest}
+                    <div data-restNum="${nextId}"></div>
+                </div>
+                <form class="textinput">
+                <textarea row="5" class="uk-input" type="text" placeholder="Comments">${nextComment}</textarea>
+                <div class="stars" action="">
+                    <input class="star star-5 save" id="${i}star-5" type="radio" name="star${i}" value="5"/>
+                    <label class="star star-5" for="${i}star-5"></label>
+                    <input class="star star-4 save" id="${i}star-4" type="radio" name="star${i}" value="4"/>
+                    <label class="star star-4" for="${i}star-4"></label>
+                    <input class="star star-3 save" id="${i}star-3" type="radio" name="star${i}" value="3"/>
+                    <label class="star star-3" for="${i}star-3"></label>
+                    <input class="star star-2 save" id="${i}star-2" type="radio" name="star${i}" value="2"/>
+                    <label class="star star-2" for="${i}star-2"></label>
+                    <input class="star star-1 save" id="${i}star-1" type="radio" name="star${i}" value="1"/>
+                    <label class="star star-1" for="${i}star-1"></label>
+                </div>
+                <div class="displayC">
+                <button class="restSubmit btn uk-button uk-button-default">Submit</button>
+                <div data-lat="${lat}"></div>
+                <button class="maps btn uk-button uk-button-default">Get Directions Again!</button>
+                <div data-lon="${lon}"></div>
+                </div>
+                </form>
+                </section>`;
+            $('#allRestPrint').append(inputCard);
+            if (nextRating == 5) {
+                var starPop = i + "star-5";
+                $('#' + starPop).prop("checked", "checked");
+            } else if (nextRating == 4) {
+                var starPop = i + "star-4";
+                $('#' + starPop).prop("checked", "checked");
+            } else if (nextRating == 3) {
+                var starPop = i + "star-3";
+                $('#' + starPop).prop("checked", "checked");
+            } else if (nextRating == 2) {
+                var starPop = i + "star-2";
+                $('#' + starPop).prop("checked", "checked");
+            } else if (nextRating == 1) {
+                var starPop = i + "star-1";
+                $('#' + starPop).prop("checked", "checked");
+            };
         }
-    }
+        i++;
+    });
+}
+
+var popRecipes = function () {
+    var storedLength = Object.keys(pullLocalRec).length;
+    var lastId = pullLocalRec.last;
+    var mostRecentName = pullLocalRec[lastId].name;
+    var mostRecentCom = pullLocalRec[lastId].comment;
+    var mostRecentRate = pullLocalRec[lastId].rating;
+    console.log(mostRecentName);
+    $('#restPrint').text(mostRecentName);
+    $('#restPrint').append('<div class="restId" data-restID="' + lastId + '"></div>')
+    var keyArr = Object.keys(pullLocalRec);
+    var i = 100;
+    keyArr.forEach(element => {
+        console.log(element);
+        if (element !== "last") {
+            var nextId = element;
+            var nextRest = pullLocalRec[element].name;
+            var nextComment = pullLocalRec[element].comment;
+            console.log(nextComment);
+            var nextRating = pullLocalRec[element].rating;
+            //Yes I'm using template literal for this
+            var inputCard = `<section class="cardC uk-margin">
+                <div class="restId" id="restName${i}">${nextRest}
+                    <div data-restNum="${nextId}"></div>
+                </div>
+                <form class="textinput">
+                <textarea row="5" class="uk-input" type="text" placeholder="Comments">${nextComment}</textarea>
+                <div class="stars" action="">
+                    <input class="star star-5 save" id="${i}star-5" type="radio" name="star${i}" value="5"/>
+                    <label class="star star-5" for="${i}star-5"></label>
+                    <input class="star star-4 save" id="${i}star-4" type="radio" name="star${i}" value="4"/>
+                    <label class="star star-4" for="${i}star-4"></label>
+                    <input class="star star-3 save" id="${i}star-3" type="radio" name="star${i}" value="3"/>
+                    <label class="star star-3" for="${i}star-3"></label>
+                    <input class="star star-2 save" id="${i}star-2" type="radio" name="star${i}" value="2"/>
+                    <label class="star star-2" for="${i}star-2"></label>
+                    <input class="star star-1 save" id="${i}star-1" type="radio" name="star${i}" value="1"/>
+                    <label class="star star-1" for="${i}star-1"></label>
+                </div>
+                <div class="displayC">
+                <button class="recSubmit btn uk-button uk-button-default">Submit</button>
+                </div>
+                </form>
+                </section>`;
+            $('#allRecipePrint').append(inputCard);
+            if (nextRating == 5) {
+                var starPop = i + "star-5";
+                $('#' + starPop).prop("checked", "checked");
+            } else if (nextRating == 4) {
+                var starPop = i + "star-4";
+                $('#' + starPop).prop("checked", "checked");
+            } else if (nextRating == 3) {
+                var starPop = i + "star-3";
+                $('#' + starPop).prop("checked", "checked");
+            } else if (nextRating == 2) {
+                var starPop = i + "star-2";
+                $('#' + starPop).prop("checked", "checked");
+            } else if (nextRating == 1) {
+                var starPop = i + "star-1";
+                $('#' + starPop).prop("checked", "checked");
+            };
+        }
+        i++;
+    });
 }
 
 console.log(JSON.parse(localStorage.getItem('visitedRestaurants')));
 var restParsed = JSON.parse(localStorage.getItem('visitedRestaurants'));
 var recentRestId = restParsed[Object.keys(restParsed)[Object.keys(restParsed).length - 1]];
 mostRecent(recentRestId);
+popRecipes();
+// var saveLocal = function (id) {
+//     pullLocalRest.last = id
+//     localStorage.setItem("visitedRestaurants", JSON.stringify(restParsed))
+// };
 
 $('input').on('click', function () {
     console.log('star test');
@@ -87,26 +160,33 @@ $('input').on('click', function () {
 //Writing this in ID form for now, will probably switch to class to for multi rest display
 $('.restSubmit').on('click', function (event) {
     event.preventDefault();
-    var textArea = $(this).siblings(0).val();
-    var restName = $(this).parent().prev().text();
-    var restId = $(this).parent().prev().children().attr('data-restId');
-    var rating = $(this).prev().val();
-    console.log(restName, restId, textArea, rating);
-    var pullLocal = localStorage.getItem('visitedRestaurants');
-    var edited = JSON.parse(pullLocal);
-    if (pullLocal === null) {
+    var restId = $(this).parent().prev().children().data("restnum");
+    console.log(restId);
+    pullLocalRest[restId].rating = $(this).prev().val();
+    pullLocalRest[restId].comment = $(this).siblings(0).first().val();
+    console.log(pullLocalRest);
+    localStorage.setItem('visitedRestaurants', JSON.stringify(pullLocalRest));
+});
 
-    } else if (Object.keys(edited).length === 1) {
-        var firstName = Object.keys(edited)[0];
-        var firstId = edited[Object.keys(edited)[0]];
-        var newObj = new Object();
-        newObj[firstName] = [firstId, textArea, rating];
-        localStorage.setItem('visitedRestaurants', JSON.stringify(newObj));
-    } else {
-        var current = JSON.parse(localStorage.getItem('visitedRestaurants'));
-        delete current.restName;
-        current[restName] = [restId, textArea, rating];
-        localStorage.setItem('visitedRestaurants', JSON.stringify(current));
-        console.log(JSON.parse(localStorage.getItem('visitedRestaurants')));
-    }
-})
+$('.recSubmit').on('click', function (event) {
+    event.preventDefault();
+    var restId = $(this).parent().prev().children().data("restnum");
+    console.log(restId);
+    pullLocalRec[restId].rating = $(this).prev().val();
+    pullLocalRec[restId].comment = $(this).siblings(0).first().val();
+    console.log(pullLocalRec);
+    localStorage.setItem('savedMeals', JSON.stringify(pullLocalRec));
+});
+
+$('.maps').on('click', function (lat, long) {
+    var lat = $(this).prev().attr("data-lat");
+    var long = $(this).next().attr("data-lon");
+    if /*for iOS*/
+      ((navigator.platform.indexOf("iPhone") != -1) || 
+       (navigator.platform.indexOf("iPod") != -1) || 
+       (navigator.platform.indexOf("iPad") != -1))
+      window.open("maps://maps.google.com/maps?daddr="+lat+","+long+"&amp;ll=");
+  
+    else /*else use Google*/
+      window.open("https://maps.google.com/maps?daddr="+lat+","+long+"&amp;ll=");
+  });
